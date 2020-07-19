a3185dfe09e132a516309675ef91a6e2e3ffe7b0
Kullappan K <codefeedkullappan@gmail.com>
2020-07-19 21:16:12 +0530

    Updates

---
+++ precache-manifest.6fb839e9a4908f867712ca65ab573b25.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe2f2d3067c21db96ccee5ac6d55a90",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "1af3a79b902b90b08990",
+    "url": "/contact-app/static/css/2.21dd1a39.chunk.css"
+  },
+  {
+    "revision": "b6556de52e524f70b0c8",
+    "url": "/contact-app/static/css/main.484ff069.chunk.css"
+  },
+  {
+    "revision": "1af3a79b902b90b08990",
+    "url": "/contact-app/static/js/2.1fe6eef9.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/contact-app/static/js/2.1fe6eef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6556de52e524f70b0c8",
+    "url": "/contact-app/static/js/main.26875ae8.chunk.js"
+  },
+  {
+    "revision": "9f5785075ca095f4a6a7",
+    "url": "/contact-app/static/js/runtime-main.f63a745a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contact-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contact-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contact-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contact-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contact-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
